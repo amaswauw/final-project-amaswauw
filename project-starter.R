19dd1beeee68baf5fe3157299e4056bfff27af87
amaswauw <amaswauw@uw.edu>
2020-05-16 16:25:34 -0700

    Added to R file with necessary libraries and data

--- project-starter.R
+++ project-starter.R
@@ -1,5 +1,12 @@
 library("openxlsx")
 library("jsonlite")
+library("dplyr")
+library("shiny")
+library("leaflet")
+library("ggplot2")
+library("plotly")
 
-result <- read.xlsx("IPEDS_data.xlsx")
-result2 <- data.frame(fromJSON(txt = "schoolInfo.json"))
+df1 <- read.xlsx("data/IPEDS_data.xlsx")
+df2 <- data.frame(fromJSON(txt = "data/schoolInfo.json"))
+df3 <- read.csv("data/Most-Recent-Cohorts-All-Data-Elements.csv", 
+                stringsAsFactors = FALSE)
