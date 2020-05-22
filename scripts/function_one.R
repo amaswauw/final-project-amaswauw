41cb9783ce233d7ac7e13e0aa3a06e17eb9df3de
amaswauw <amaswauw@uw.edu>
2020-05-21 17:36:29 -0700

    Summary table #1 done

--- scripts/function_one.R
+++ scripts/function_one.R
@@ -6,3 +6,19 @@ df1 <- read.xlsx("data/IPEDS_data.xlsx")
 df2 <- data.frame(fromJSON(txt = "data/schoolInfo.json"))
 df3 <- read.csv("data/Most-Recent-Cohorts-All-Data-Elements.csv", 
                 stringsAsFactors = FALSE)
+join_result <- inner_join(df3, df1, by = c("INSTNM" = "Name"))
+join_result2 <- left_join(join_result, df2, by = c("INSTNM" = "displayName"))
+summary_table <- join_result2 %>%
+  select("INSTNM", "SAT_AVG_ALL", "act.avg", "hs.gpa.avg",
+         "Percent.of.total.enrollment.that.are.American.Indian.or.Alaska.Native",
+         "Percent.of.total.enrollment.that.are.Asian",
+         "Percent.of.total.enrollment.that.are.Black.or.African.American",
+         "Percent.of.total.enrollment.that.are.Hispanic/Latino",
+         "Percent.of.total.enrollment.that.are.White",
+         "Percent.of.total.enrollment.that.are.two.or.more.races",
+         "Percent.of.total.enrollment.that.are.Race/ethnicity.unknown",
+         "Percent.of.total.enrollment.that.are.Nonresident.Alien",
+         "Percent.of.total.enrollment.that.are.Asian/Native.Hawaiian/Pacific.Islander",
+         "Percent.of.total.enrollment.that.are.women",
+         "overallRank",
+         "acceptance.rate")
