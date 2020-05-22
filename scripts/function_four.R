bab9b0446affb9fb2c859375a424952be2ac7c1d
Andrew Michaels <amicha@uw.edu>
2020-05-21 19:27:19 -0700

    add scatter plot

--- scripts/function_four.R
+++ scripts/function_four.R
@@ -1,8 +1,63 @@
 library("jsonlite")
 library("openxlsx")
 library("dplyr")
+library("ggplot2")
+library("plotly")
+library("styler")
+library("lintr")
 
 df1 <- read.xlsx("data/IPEDS_data.xlsx")
 df2 <- data.frame(fromJSON(txt = "data/schoolInfo.json"))
-df3 <- read.csv("data/Most-Recent-Cohorts-All-Data-Elements.csv", 
-                stringsAsFactors = FALSE)
+df3 <- read.csv("data/Most-Recent-Cohorts-All-Data-Elements.csv",
+  stringsAsFactors = FALSE
+)
+
+table_join <- inner_join(df1, df3, by = c("Name" = "INSTNM"))
+table_join2 <- left_join(
+  table_join,
+  df2,
+  by = c("Tuition.and.fees,.2013-14" = "tuition")
+)
+
+
+# Table with tution averages per state
+tution_table <- table_join2 %>%
+  select(
+    Name = "Name",
+    State = "State.abbreviation",
+    Tuition = "Tuition.and.fees,.2013-14"
+  ) %>%
+  group_by(State) %>%
+  summarize(Average_Tuition = mean(Tuition, na.rm = T)) %>%
+  arrange(-Average_Tuition)
+
+
+
+# Produce scatter plot of average tuition of all universities
+# in each state.
+scatter_plot <- function(df) {
+  plot_ly(
+    data = df,
+    x = ~Average_Tuition,
+    y = ~State,
+    color = ~Average_Tuition,
+    type = "scatter"
+  ) %>%
+    layout(
+      title = "Average Tuition of all Universities per State",
+      xaxis = list(
+        title = "Average Tuition"
+      ),
+      yaxis = list(
+        title = "",
+        showticklabels = F,
+        showgrid = F
+      ),
+      showlegend = F,
+      legend = list(
+        title = list(text = "<b> Trend </b>")
+      )
+    )
+}
+
+chart_one <- scatter_plot(tution_table)
