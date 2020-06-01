0544e4bcbc6e890d41c98c82f285eeeaa82f6468
Andrew Michaels <amicha@uw.edu>
2020-05-31 20:44:59 -0700

    add scatter plot

--- app_server.R
+++ app_server.R
@@ -10,7 +10,7 @@ library(ggplot2)
 
 
 
-# Tuition
+
 
 server <- shinyServer(function(input, output) {
   #render the introduction page
@@ -20,6 +20,31 @@ server <- shinyServer(function(input, output) {
   
   
   # Tuition
+  #x <- reactive ({
+  #  final_tuition_table[, input$xaxis]
+  #})  
+  
+  #y <- reactive({
+  #  final_tuition_table[, input$yaxis]
+  #})
+  #output$scatter_plot <- renderPlotly (
+  #  p <- plot_ly(
+  #    x = x(),
+  #    y = y(),
+  #    type = "scatter"
+  #  )
+  #)
+  data <- final_tuition_table
+  #search = "Washington"
+  graph_var = "In_state"
+  
+  
+  
+  output$scatter_plot <- renderPlotly ({
+    return(draw_scatter(final_tuition_table, input$yaxis))
+  })
+  
+  
   
   
   # Academics
@@ -34,52 +59,7 @@ server <- shinyServer(function(input, output) {
   })
 
   
-#x <- reactive ({
-#  final_tuition_table[, input$xaxis]
-#})  
 
-#y <- reactive({
-#  final_tuition_table[, input$yaxis]
-#})
-#output$scatter_plot <- renderPlotly (
-#  p <- plot_ly(
-#    x = x(),
-#    y = y(),
-#    type = "scatter"
-#  )
-#)
-#data <- final_tuition_table
-#search = "Washington"
-#graph_var = "In_state"
-  
-  draw_scatter <- function(data, graph_var) {
-    
-    ymax <- max(data[,graph_var]) * 1.5
-    
-   # filtered_data <- data %>%
-    #  filter(data$State == search)
-    graph_df <- data %>%
-      select("State", graph_var)
-    graph <- plot_ly(
-      data = graph_df,
-      x=~State,
-      y=~graph_df[,graph_var],
-      type ="scatter",
-      mode = "markers",
-      marker = list(
-        size = 10
-      )
-    ) %>% 
-      layout( 
-             yaxis = list(range = c(0, ymax), title = graph_var)
-      )
-    
-    return(graph)
-  }
-  
-  output$scatter_plot <- renderPlotly ({
-    return(draw_scatter(final_tuition_table, input$yaxis))
-  })
   
 # Academics
 
@@ -96,3 +76,45 @@ server <- shinyServer(function(input, output) {
 
 # Ethnicity
 
+
+draw_scatter <- function(data, graph_var) {
+  
+  #ymax <- max(data[,graph_var]) * 1.5
+  
+  # filtered_data <- data %>%
+  #  filter(data$State == search)
+  
+  graph_df <- data %>%
+    select("State", graph_var)
+  #View(graph_df)
+  #print(graph_df[,graph_var])
+  if (graph_var == "In_state") {
+  graph <- plot_ly(
+    data = graph_df,
+    x=~State,
+    y=~In_state,
+    type ="scatter",
+    mode = "markers",
+    marker = list(
+      size = 10
+    )
+  ) 
+  } else {
+    graph <- plot_ly(
+      data = graph_df,
+      x=~State,
+      y=~Out_of_State,
+      type ="scatter",
+      mode = "markers",
+      marker = list(
+        size = 10
+      )
+    ) 
+  }
+  #%>% 
+  #ayout( 
+  #yaxis = list(range = c(0, ymax), title = graph_var)
+  #)
+  
+  return(graph)
+}
--- app_ui.R
+++ app_ui.R
@@ -56,7 +56,60 @@ states <- list(
 
 
 # Tuition
+# Loading in the necessary dataframes.
+df1 <- read.xlsx("data/IPEDS_data.xlsx")
+df2 <- data.frame(fromJSON(txt = "data/schoolInfo.json"))
+df3 <- read.csv("data/Most-Recent-Cohorts-All-Data-Elements.csv",
+                stringsAsFactors = FALSE
+)
 
+table_join <- right_join(df1, df3, by = c("Name" = "INSTNM"))
+table_join2 <- left_join(
+  table_join,
+  df2,
+  by = c("Total.price.for.in-state.students.living.on.campus.2013-14" = "tuition"))
+
+
+# Table with tution averages per state.
+tuition_table <- table_join2 %>%
+  select(Name = "Name",
+         State = "STABBR",
+         In_State = "Total.price.for.in-state.students.living.on.campus.2013-14",
+         Out_of_State = "Total.price.for.out-of-state.students.living.on.campus.2013-14") %>%
+  group_by(State) %>%
+  summarize(In_state = mean(In_State, na.rm = T), Out_of_State = mean(Out_of_State, na.rm = T)) %>%
+  arrange(-In_state)
+
+#Remove na values from tuition table
+final_tuition_table <- na.omit(tuition_table)
+
+page_one <- tabPanel(
+  "Tuition Visualization",
+  titlePanel("Average Tuition per State"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "yaxis",
+        label = "Tuition",
+        choices = list("In_state", "Out_of_State"),
+        selected = "In_state"
+      ),
+      #selectInput(
+      # inputId = "xaxis",
+      #label = "State",
+      #choices = states,
+      #selected = "Washington"
+      #)
+    ),
+    mainPanel(
+      h1("Tuition Chart"),
+      p("This chart illustrates the average in state tuition and
+        average out of state tuition per state"),
+      plotlyOutput(outputId = "scatter_plot")
+    )
+  )
+)
 
 # Academics
 sidebar_content <- sidebarPanel(
@@ -95,6 +148,7 @@ ui <- navbarPage(
   "University Statistics in the US",
   
   #introduction page of the application
+  page_one,
   tabPanel("Introduction",
            mainPanel(uiOutput("introduction"))),
   academic,
