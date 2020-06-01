56a8be4f7c23178d3eb53cab534f630a0a0d92a2
amaswauw <amaswauw@uw.edu>
2020-06-01 00:39:48 -0700

    Fixed linter errors

--- app_server.R
+++ app_server.R
@@ -87,37 +87,39 @@ server <- shinyServer(function(input, output) {
   output$summarystates <- DT::renderDataTable({
     all_states_summary
   })
-  
+
   output$pie_chart <- renderPlotly({
-    data_chart <- join_result2 %>% 
-      group_by(STABBR) %>% 
-      summarize(ave_American_Indian =
-                  mean(
-                    Percent.of.total.enrollment.that.are.American.Indian.or.Alaska.Native,
-                    na.rm = TRUE),
-                ave_Asian = mean(Percent.of.total.enrollment.that.are.Asian,
-                                 na.rm = TRUE),
-                ave_African_American =
-                  mean(
-                    Percent.of.total.enrollment.that.are.Black.or.African.American,
-                    na.rm = TRUE),
-                ave_Latino =
+    data_chart <- join_result2 %>%
+      group_by(STABBR) %>%
+      summarize(
+        ave_American_Indian =
+          mean(
+    Percent.of.total.enrollment.that.are.American.Indian.or.Alaska.Native,
+    na.rm = TRUE),
+    ave_Asian = mean(Percent.of.total.enrollment.that.are.Asian,
+                     na.rm = TRUE),
+    ave_African_American =
+      mean(
+        Percent.of.total.enrollment.that.are.Black.or.African.American,
+        na.rm = TRUE),
+    ave_Latino =
                   mean(
                     `Percent.of.total.enrollment.that.are.Hispanic/Latino`,
                     na.rm = TRUE),
-                ave_White = mean(Percent.of.total.enrollment.that.are.White, na.rm = TRUE),
+                ave_White = mean(Percent.of.total.enrollment.that.are.White,
+                                 na.rm = TRUE),
                 ave_more_race =
                   mean(
                     Percent.of.total.enrollment.that.are.two.or.more.races,
                     na.rm = TRUE),
-                ave_unknown =
-                  mean(
-                    `Percent.of.total.enrollment.that.are.Race/ethnicity.unknown`,
+    ave_unknown =
+      mean(
+        `Percent.of.total.enrollment.that.are.Race/ethnicity.unknown`,
                     na.rm = TRUE),
-                ave_Islander =
-                  mean(
-                    `Percent.of.total.enrollment.that.are.Asian/Native.Hawaiian/Pacific.Islander`,
-                    na.rm = TRUE)) %>% 
+    ave_Islander =
+      mean(
+`Percent.of.total.enrollment.that.are.Asian/Native.Hawaiian/Pacific.Islander`,
+                    na.rm = TRUE)) %>%
       filter(STABBR == input$diversityInput)
     return(draw_pie(data_chart, ethnicity_categories))
   })
@@ -132,34 +134,25 @@ ethnicity_categories <- c("American Indian / Alaska Native",
                           "Unknown Ethnicity",
                           "Asian / Native Hawaiian / Pacific Islander")
 
-
-
-
-
-
-
 #Create the tuition scatter plot
 draw_scatter <- function(data, graph_var) {
-
   graph_df <- data %>%
     select("State", graph_var)
   if (graph_var == "In_state") {
-  graph <- plot_ly(
-    data = graph_df,
-    x = ~State,
-    y = ~In_state,
-    type = "scatter",
-    mode = "markers",
-    marker = list(
-      size = 10
-    )
-  ) %>% 
-  layout( 
-    title = "In-State Tuition vs State",
-    xaxis = list(title = "State"),
-    yaxis = list(title = "In-State Tuition")
-  ) 
-
+    graph <- plot_ly(
+      data = graph_df,
+      x = ~State,
+      y = ~In_state,
+      type = "scatter",
+      mode = "markers",
+      marker = list(
+        size = 10
+      )
+    ) %>%
+      layout(title = "In-State Tuition vs State",
+             xaxis = list(title = "State"),
+             yaxis = list(title = "In-State Tuition")
+             )
   } else {
     graph <- plot_ly(
       data = graph_df,
@@ -170,8 +163,8 @@ draw_scatter <- function(data, graph_var) {
       marker = list(
         size = 10
       )
-    ) %>% 
-    layout( 
+    ) %>%
+    layout(
       title = "Out-of-State Tuition vs State",
       xaxis = list(title = "State"),
       yaxis = list(title = "Out-of-State Tuition")
@@ -274,7 +267,6 @@ text_summary <- function(data, search) {
   }
 }
 
-
 draw_bar <- function(data, search, graph_var) {
   filtered_data <- data %>%
     filter(data$STABBR == search)
@@ -293,11 +285,10 @@ draw_bar <- function(data, search, graph_var) {
   return(graph)
 }
 
-
 # Ethnicity
-draw_pie <- function(data, category){
-  div_percentages <- c(data$ave_American_Indian, 
-                       data$ave_Asian, 
+draw_pie <- function(data, category) {
+  div_percentages <- c(data$ave_American_Indian,
+                       data$ave_Asian,
                        data$ave_African_American,
                        data$ave_Latino,
                        data$ave_White,
@@ -309,9 +300,9 @@ draw_pie <- function(data, category){
   pie_plot <- plot_ly(
     pie_df,
     labels = ~categories,
-    values = ~percentages, 
+    values = ~percentages,
     type = "pie") %>%
-    layout(legend = list(orientation = 'h'))
+    layout(legend = list(orientation = "h"))
   return(pie_plot)
 }
 
